2f44071c8974ff9e4f61d1c63aa6552b4db67683
vedat73 <vedat.ozlu@outlook.com>
2023-07-18 15:08:15 +0300

    Initial Commit

---
+++ FruitBook/FruitBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitBookApp.swift
+//  FruitBook
+//
+//  Created by Vedat Ozlu on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
